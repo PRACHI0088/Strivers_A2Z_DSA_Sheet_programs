44de0a0807995d020643867a8fac54241ae5547d
PRACHI <99760316+PRACHI0088@users.noreply.github.com>
2023-10-18 22:03:38 +0530

    Add files via upload

---
+++ Q15_(b)_Optimal_ans__if_sum_is_present_or_not.cpp
@@ -0,0 +1,52 @@
+/*Q 15 (B) BETTER METHOD: by using 2 pointer approach and using the sort function of stl library.
+ Check if a pair with given sum exists in Array
+Problem Statement: Given an array of integers arr[] and an integer target.*/
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+string read(int n, vector<int> book, int target)
+{ int sum=0,i,j;
+    i=0;
+    j=n-1;
+    sort(book.begin(),book.end());
+        while(i<j)
+        {  sum=book[i]+book[j];
+            if(sum<target)
+            {
+                i++;
+            }
+            else
+            if(sum>target)
+            {
+                j--;
+            }
+            else
+            if(sum==target)
+            {
+                return "YES";
+                break;
+            }
+        }
+    return "NO";
+}
+
+int main()
+{	int n,k;
+	cout<<"enter the size of the array:\n";
+	cin>>n;
+	cout<<"enter the target sum:";
+	cin>>k;
+	vector<int> arr(n);
+	cout<<"enter the array elemments: \n";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+
+	cout<<"the target is present ? : ";
+	cout<<read(n,arr,k);
+
+	return 0;
+}
